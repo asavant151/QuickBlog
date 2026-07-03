f64495bb38d405ac39ddb496e81873b1b5b319ac
asavant151 <your-email@example.com>
2026-07-03 22:20:29 +0530

    header and footer changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -20,10 +20,10 @@ const Header = () => {
   return (
     <div className='sm:mx-16 xl:mx-24 relative'>
       <div className='text-center mt-20 mb-8'>
-        <div className='inline-flex items-center justify-center gap-4 px-6 py-1.5 mb-4 border border-primary/40 bg-primary/10 rounded-full text-sm text-primary'>
+        {/* <div className='inline-flex items-center justify-center gap-4 px-6 py-1.5 mb-4 border border-primary/40 bg-primary/10 rounded-full text-sm text-primary'>
             <p>New: AI feature integrated</p>
             <img src={assets.star_icon} className='w-2.5' alt="star" />
-        </div>
+        </div> */}
         <h1 className='text-3xl sm:text-6xl font-semibold sm:leading-16 text-gray-700'>Your own <span className='text-primary'>blogging</span> <br /> platform.</h1>
 
         <p className='my-6 sm:my-8 max-w-2xl m-auto max-sm:text-xs text-gray-500'>This is your space to think out loud, to share what matters, and to write without filters. Whether it’s one word or a thousand, your story starts right here.</p>
