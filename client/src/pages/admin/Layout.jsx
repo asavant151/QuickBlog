a2e857b683028162f8ef8e1dd49658378bd5e163
asavant151 <your-email@example.com>
2026-07-04 13:53:02 +0530

    navigation issue solve

--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -16,7 +16,7 @@ const Layout = () => {
   return (
     <>
       <div className='flex items-center justify-between py-2 h-[70px] sm:px-12 border-b border-gray-200'>
-        <img src={assets.logo} alt="logo" className='w-32 sm:w-40 cursor-pointer' onClick={() => navigate('/')} />
+        <img src={assets.logo} alt="logo" className='w-32 sm:w-40 cursor-pointer' onClick={() => navigate('/admin')} />
         <button className='text-sm px-8 py-2 bg-primary text-white rounded-full cursor-pointer' onClick={logout}>Logout</button>
       </div>
       <div className='flex h-[calc(100vh-70px)]'>
